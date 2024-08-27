117795064aa8ddac847ca527cc80b14e6c352403
JAVISU007 <javisu0007@gmail.com>
2024-08-26 20:17:39 -0500

    Add files via upload
    CARGA INICIAL

---
+++ script.js
@@ -0,0 +1,60 @@
+textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+const btnCopiar = document.getElementById("copiar");
+const h3Mensaje = document.getElementById("mensaje");
+const pTexto = document.getElementById("texto");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
+    
+    mostrarResultado();
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+
+    mostrarResultado();
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+    return stringDesencriptada;
+}
+
+function mostrarResultado() {
+    if (mensaje.value.trim() !== "") {
+        h3Mensaje.style.display = "none";
+        pTexto.style.display = "none";
+        btnCopiar.style.display = "block";
+    }
+}
+
+function copiarTexto() {
+    mensaje.select();
+    document.execCommand("copy");
+    alert("Texto copiado al portapapeles");
+}
